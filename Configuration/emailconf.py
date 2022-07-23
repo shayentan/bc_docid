6b56bf55b12ddd78942e81f32ed92b728c33eb29
shayentan <shayentanupadhyay@gmail.com>
2022-07-23 18:11:41 +0530

    commit conf

---
+++ Configuration/emailconf.py
@@ -0,0 +1,11 @@
+
+class conf_email:
+    imaphost ="mail.justdial.com"
+    username = "shayentan.upadhyay"
+    app_password = "Shiv&108"
+    smtp_ssl_host = "mail.justdial.com"
+    smtp_ssl_port = 465
+    sender = "shayentan.upadhyay@justdial.com"
+    targets = ["bharatsharma@justdial.com", "shayentan.upadhyay@justdial.com",
+               "product.testers@justdial.com", "pravin.kucha@justdial.com", "ashleydsilva@justdial.com"]
+
